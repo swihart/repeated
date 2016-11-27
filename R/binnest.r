6d240bac4ce3dd4d5cb10362ce4b82d3e41662aa
swihart <bruce.swihart@gmail.com>
2016-11-27 07:46:31 -0500

    dontrun back in binnest after many reverts - does travis ci succeed?

--- R/binnest.r
+++ R/binnest.r
@@ -103,7 +103,7 @@
 ##' in Medicine 18, 947-960.
 ##' @keywords models
 ##' @examples
-##' 
+##' \dontrun{
 ##' #y <- rbind(matrix(rbinom(20,1,0.6), ncol=4),
 ##' #	matrix(rbinom(20,1,0.4), ncol=4))
 ##' y <- matrix(c(1,1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,0,1,0,1,1,0,1,0,
@@ -117,7 +117,7 @@
 ##' binnest(reps, mu=~treatment, preg=c(1,-1), pre1=1)
 ##' # second level random effect only
 ##' binnest(reps, mu=~treatment, preg=c(1,-1), pre2=1)
-##' 
+##' }
 ##' @export binnest
 binnest <- function(response, totals=NULL, nest=NULL, ccov=NULL, tvcov=NULL,
 	mu=~1, re1=~1, re2=~1, preg=NULL, pre1=NULL, pre2=NULL,
