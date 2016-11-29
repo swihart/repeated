e130fdd831fe0fbd4cdbf0625dc3f5ed71fb04c4
swihart <bruce.swihart@gmail.com>
2016-11-28 22:03:51 -0500

    nbkal remove dontrun from example

--- R/nbkal.r
+++ R/nbkal.r
@@ -76,14 +76,14 @@
 ##' Lambert, P. (1996) Biometrics 52, 50-55.
 ##' @keywords models
 ##' @examples
-##' \dontrun{
+##' 
 ##' y <- matrix(rnbinom(20,5,0.5), ncol=5)
 ##' times <- matrix(rep(seq(10,50,by=10),4), ncol=5, byrow=TRUE)
 ##' y0 <- matrix(rep(rnbinom(5,5,0.5),4), ncol=5, byrow=TRUE)
 ##' mu <- function(p) p[1]*log(y0)+(times<30)*p[2]*
 ##' 	(times-30)+(times>30)*p[3]*(times-30)
 ##' nbkal(y, preg=c(1.3,0.008,-0.05), times=times, pdep=1.2, mu=mu)
-##' }
+##' 
 ##' @aliases nbkal print.nbkal
 ##' @export nbkal
 nbkal <- function(response, times, mu, preg, pdepend, kalman=TRUE,
