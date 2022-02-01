5ddaa7e7d286fb5fec9d46926220310ad41152e2
Bruce Swihart <swihart@users.noreply.github.com>
2022-01-31 22:16:33 -0500

    add library(repeated) to examples.R
    (unreal -- makes me wonder if devtools::run_examples() takes care of library(repeated) but not library(rmutil) )

--- memcheck/examples.R
+++ memcheck/examples.R
@@ -1,4 +1,5 @@
 library(rmutil)
+library(repeated)
 
 # Code to be run with
 #   R -d "valgrind --tool=memcheck --leak-check=full --error-exitcode=1" --vanilla < tests/thisfile.R
@@ -6,6 +7,7 @@ library(rmutil)
 
 # Code to be run with
 #   R -d "valgrind --tool=memcheck --leak-check=full --track-origins=yes" --vanilla < tests/thisfile.R
+
 dose <- c(9,12,4,9,11,10,2,11,12,9,9,9,4,9,11,9,14,7,9,8)
 y <- c(8.674419, 11.506066, 11.386742, 27.414532, 12.135699,  4.359469,
        1.900681, 17.425948,  4.503345,  2.691792,  5.731100, 10.534971,
