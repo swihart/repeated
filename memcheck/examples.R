713412d9eeb1c5b34f38cd3b2296229fb5dd3aaf
swihart <bruce.swihart@gmail.com>
2022-02-01 00:34:19 -0500

    valgrind exampes memcheck

--- memcheck/examples.R
+++ memcheck/examples.R
@@ -8,17 +8,61 @@ library(repeated)
 # Code to be run with
 #   R -d "valgrind --tool=memcheck --leak-check=full --track-origins=yes" --vanilla < tests/thisfile.R
 
-dose <- c(9,12,4,9,11,10,2,11,12,9,9,9,4,9,11,9,14,7,9,8)
-y <- c(8.674419, 11.506066, 11.386742, 27.414532, 12.135699,  4.359469,
-       1.900681, 17.425948,  4.503345,  2.691792,  5.731100, 10.534971,
-      11.220260,  6.968932,  4.094357, 16.393806, 14.656584,  8.786133,
-      20.972267, 17.178012)
-resp <- restovec(matrix(y, nrow=4, byrow=TRUE), name="y")
-reps <- rmna(resp, tvcov=tvctomat(matrix(dose, nrow=4, byrow=TRUE), name="dose"))
+# dose <- c(9,12,4,9,11,10,2,11,12,9,9,9,4,9,11,9,14,7,9,8)
+# y <- c(8.674419, 11.506066, 11.386742, 27.414532, 12.135699,  4.359469,
+#        1.900681, 17.425948,  4.503345,  2.691792,  5.731100, 10.534971,
+#       11.220260,  6.968932,  4.094357, 16.393806, 14.656584,  8.786133,
+#       20.972267, 17.178012)
+# resp <- restovec(matrix(y, nrow=4, byrow=TRUE), name="y")
+# reps <- rmna(resp, tvcov=tvctomat(matrix(dose, nrow=4, byrow=TRUE), name="dose"))
 
 # same linear normal model with random normal intercept fitted four ways
 # compare with growth::elliptic(reps, model=~dose, preg=c(0,0.6), pre=4)
 # glmm(y~dose, nest=individuals, data=reps)
 # gnlmm(reps, mu=~dose, pmu=c(8.7,0.25), psh=3.5, psd=3)
-gnlmix(reps, mu=~a+b*dose+rand, random="rand", pmu=c(8.7,0.25),
-	pshape=3.44, pmix=2.3)
+#gnlmix(reps, mu=~a+b*dose+rand, random="rand", pmu=c(8.7,0.25),
+#            pshape=3.44, pmix=2.3)
+
+
+library(repeated)
+
+dose <- c(9,12,4,9,11,10,2,11,12,9,9,9,4,9,11,9,14,7,9,8)
+y <- c(8.674419, 11.506066, 11.386742, 27.414532, 12.135699,  4.359469,
+       1.900681, 17.425948,  4.503345,  2.691792,  5.731100, 10.534971,
+       11.220260,  6.968932,  4.094357, 16.393806, 14.656584,  8.786133,
+       20.972267, 17.178012)
+id <- rep(1:4, each=5)
+
+
+beg.jim <- Sys.time()
+jim <- gnlmix(y, mu=~a+b*dose+rand, random="rand", nest=id, pmu=c(a=8.7,b=0.25),
+              pshape=3.44, pmix=2.3)
+end.jim <- Sys.time()
+time.jim <- end.jim - beg.jim
+
+beg.dotc <- Sys.time()
+dotc <- gnlmix_dotc(y, mu=~a+b*dose+rand, random="rand", nest=id, pmu=c(a=8.7,b=0.25),
+                    pshape=3.44, pmix=2.3)
+end.dotc <- Sys.time()
+time.dotc <- end.dotc - beg.dotc
+
+
+
+beg.dotcall <- Sys.time()
+dotcall <- gnlmix_dotcall(y, mu=~a+b*dose+rand, random="rand", nest=id, pmu=c(a=8.7,b=0.25),
+                          pshape=3.44, pmix=2.3)
+end.dotcall <- Sys.time()
+time.dotcall <- end.dotcall - beg.dotcall
+
+
+dotcall$coef
+dotc$coef
+jim$coef
+
+dotcall$maxlike
+dotc$maxlike
+jim$maxlike
+
+time.dotcall
+time.dotc
+time.jim
--- src/repeated_init.c
+++ src/repeated_init.c
@@ -17,7 +17,7 @@ extern void kserie_c(void *, void *, void *, void *, void *, void *, void *, voi
 extern void pginvgauss_c(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
 extern void ppowexp_c(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
 extern void psimplex_c(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
-// extern void romberg_c(void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void romberg_c(void *, void *, void *, void *, void *, void *, void *, void *, void *);
 
 static const R_CMethodDef CEntries[] = {
     {"countfb_c",    (DL_FUNC) &countfb_c,    21},
@@ -33,15 +33,15 @@ static const R_CMethodDef CEntries[] = {
     {"pginvgauss_c", (DL_FUNC) &pginvgauss_c, 10},
     {"ppowexp_c",    (DL_FUNC) &ppowexp_c,    10},
     {"psimplex_c",   (DL_FUNC) &psimplex_c,   10},
-    // {"romberg_c",    (DL_FUNC) &romberg_c,     9},
+    {"romberg_c",    (DL_FUNC) &romberg_c,     9},
     {NULL, NULL, 0}
 };
 
 
 /* .Call()  */
-extern void romberg_sexp(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
-// SEXP romberg_sexp(SEXP fcn, SEXP a, SEXP b, SEXP len, SEXP eps,
-//                   SEXP pts, SEXP max, SEXP err, SEXP envir);
+//extern void romberg_sexp(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+SEXP romberg_sexp(SEXP fcn, SEXP a, SEXP b, SEXP len, SEXP eps,
+                   SEXP pts, SEXP max, SEXP err, SEXP envir);
 
 static const R_CallMethodDef callMethods[]  = {
   {"romberg_sexp", (DL_FUNC) &romberg_sexp, 9},
